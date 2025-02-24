1f1abc78561379afd6cc0b1c1e4ab2c785dd29e6
annalisekimura <kimuraannalise@gmail.com>
2025-02-24 16:12:43 -0500

    ios and android fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'widget_tree.dart';
 import './pages/home_page.dart';
 import './pages/login_page.dart';
 
 Future<void> main() async {
+  
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  
   
   runApp(const MyApp());
 }
